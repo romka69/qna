e418758d10910e98059868583791d942b097e14b
roman <romka69tver@gmail.com>
2019-03-24 20:53:29 +0300

    add model vote

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe User, type: :model do
   it { should have_many(:questions).with_foreign_key('author_id').dependent(:nullify) }
   it { should have_many(:answers).with_foreign_key('author_id').dependent(:nullify) }
   it { should have_many(:badges).dependent(:nullify) }
+  it { should have_many(:votes).dependent(:destroy) }
 
   it { should validate_presence_of :email }
   it { should validate_presence_of :password }
