059567e8517c271b77a4f41f4170b55a9d0f4d22
roman <romka69tver@gmail.com>
2019-04-10 20:43:27 +0300

    add create update destroy to api answers

--- spec/api/v1/answer_spec.rb
+++ spec/api/v1/answer_spec.rb
@@ -104,4 +104,125 @@ describe 'Answers API', type: :request do
       end
     end
   end
+
+  describe 'POST /api/v1/questions/:id/answers' do
+    let(:user) { create :user }
+    let!(:question) { create :question, author: user }
+
+    let(:api_path) { "/api/v1/questions/#{question.id}/answers" }
+    let(:access_token) { create :access_token }
+    let(:headers) { { "ACCEPT" => "application/json" } }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :post }
+    end
+
+    context 'Create answer valid attributes' do
+      before { post api_path, params: {
+          access_token: access_token.token,
+          answer: { body: 'body', question: question } }, headers: headers }
+
+      it_behaves_like 'Request status 201'
+
+      it 'add answer in db' do
+        expect(Answer.count).to eq 1
+      end
+
+      it 'return fields of new object' do
+        %w[id body created_at updated_at].each do |attr|
+          expect(json['answer'].has_key?(attr)).to be_truthy
+        end
+      end
+    end
+
+    context 'Create answer invalid attributes' do
+      before { post api_path, params: {
+          access_token: access_token.token,
+          answer: { body: nil, question: question } }, headers: headers }
+
+      it 'return 422' do
+        expect(response.status).to eq 422
+      end
+
+      it 'return error in object' do
+        %w[body].each do |attr|
+          expect(json.has_key?(attr)).to be_truthy
+        end
+      end
+    end
+  end
+
+  describe 'PATCH /api/v1/answers/:id' do
+    let(:user) { create :user }
+    let(:question) { create :question, author: user }
+    let(:answer) { create :answer, question: question, author: user}
+
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+    let(:access_token) { create :access_token }
+    let(:headers) { { "ACCEPT" => "application/json" } }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :patch }
+    end
+
+    context 'Update question valid attributes' do
+      before { patch api_path, params: {
+          access_token: access_token.token,
+          answer: { body: 'Edit body' } }, headers: headers }
+
+      it_behaves_like 'Request status success'
+
+      it 'return fields with modifier data' do
+        %w[id body created_at updated_at].each do |attr|
+          expect(json['answer'].has_key?(attr)).to be_truthy
+        end
+      end
+
+      it 'check modifier field' do
+        expect(json['answer']['body']).to eq 'Edit body'
+      end
+    end
+
+    context 'Update question invalid attributes' do
+      before { patch api_path, params: {
+          access_token: access_token.token,
+          answer: { body: nil } }, headers: headers }
+
+      it 'return 422' do
+        expect(response.status).to eq 422
+      end
+
+      it 'return error in object' do
+        %w[body].each do |attr|
+          expect(json.has_key?(attr)).to be_truthy
+        end
+      end
+    end
+  end
+
+  describe 'DELETE /api/v1/answers/:id' do
+    let(:user) { create :user }
+    let(:question) { create :question, author: user }
+    let!(:answer) { create :answer, question: question, author: user}
+
+    let(:api_path) { "/api/v1/answers/#{answer.id}" }
+    let(:access_token) { create :access_token }
+    let(:headers) { { "ACCEPT" => "application/json" } }
+
+    it_behaves_like 'API Authorizable' do
+      let(:method) { :delete }
+    end
+
+    context 'Authorized' do
+      before { delete api_path, params: {
+          access_token: access_token.token,
+          answer: answer }, headers: headers }
+
+      it_behaves_like 'Request status success'
+
+      it 'del answer in db' do
+        expect(Answer.count).to eq 0
+      end
+    end
+  end
 end
