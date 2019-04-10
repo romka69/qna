9483450c6b754c9c11ef699db77179cf6c201c5a
roman <romka69tver@gmail.com>
2019-04-10 21:10:36 +0300

    refactoring specs

--- spec/support/shared/return_fields.rb
+++
@@ -1,7 +0,0 @@
-shared_examples_for 'Returns fields' do
-  it 'all public fields' do
-    fields.each do |attr|
-      expect(resource_response[attr]).to eq resource_name.send(attr).as_json
-    end
-  end
-end
--- spec/support/shared/returns_list_of_resource.rb
+++
@@ -1,5 +0,0 @@
-shared_examples_for 'Returns list of' do
-  it 'resource' do
-    expect(json_resource.size).to eq 2
-  end
-end
