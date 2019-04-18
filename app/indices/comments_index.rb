cd52d2e23c96860293b44910c468da648f986f5c
roman <romka69tver@gmail.com>
2019-04-19 00:23:07 +0300

    add sphinx index for question, answer, comment, user

---
+++ app/indices/comments_index.rb
@@ -0,0 +1,8 @@
+ThinkingSphinx::Index.define :comment, with: :active_record do
+  # fields
+  indexes body
+  indexes author.email, sortable: true
+
+  # attributes
+  has author_id, created_at, updated_at
+end
